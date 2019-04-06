e4a65ebc61428f860611e31be72d5d0cf91b0f6a
archetype2142 <malhotraritwick2011@gmail.com>
2019-04-06 19:49:12 +0200

    sd

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,9 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :events
   has_one_attached :featured_image
-
+  validates :name, presence: true
+  validates :description, presence: true
+  
   def featured_image_url
     if self.featured_image.attachment
       self.featured_image.attachment.service_url
