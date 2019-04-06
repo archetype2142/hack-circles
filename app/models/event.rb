3c8d1ccbb40a033dfabb3106f940f513a7dd9f95
archetype2142 <malhotraritwick2011@gmail.com>
2019-04-06 18:27:39 +0200

    nice commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :group
+  has_one_attached :featured_image
+
+  def featured_image_url
+    if self.featured_image.attachment
+      self.featured_image.attachment.service_url
+    end
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,11 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :events
+  has_one_attached :featured_image
+
+  def featured_image_url
+    if self.featured_image.attachment
+      self.featured_image.attachment.service_url
+    end
+  end
 end
