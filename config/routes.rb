795bf7a9e470d6b66341931e65a0ea8ed95af257
archetype2142 <malhotraritwick2011@gmail.com>
2019-04-06 17:09:44 +0200

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :groups
+  resources :groups do 
+  	resources :events
+  end
   root 'groups#index', as: :authenticated_root
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_06_131122) do
+ActiveRecord::Schema.define(version: 2019_04_06_133038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.datetime "date"
+    t.string "address"
+    t.bigint "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_events_on_group_id"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -54,5 +64,6 @@ ActiveRecord::Schema.define(version: 2019_04_06_131122) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "events", "groups"
   add_foreign_key "groups", "users"
 end
