8355e17b6d2f404993037f0a7b604cb0f298a42c
archetype2142 <malhotraritwick2011@gmail.com>
2019-04-07 17:30:56 +0200

    ads'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Group.find(params[:group_id]).posts
   end
 
   # GET /posts/1
